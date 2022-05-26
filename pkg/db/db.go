0429940dc64f20e40595ce75a3078d7ace8c3032
piotrostr <piotr.ostrowski@aol.com>
2022-05-26 22:37:57 +0200

    drop redundant if statement

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -10,9 +10,6 @@ import (
 func Client() *redis.Client {
 	// in the cluster only the pods can connect, hence no need for password
 	host := os.Getenv("REDIS_HOST")
-	if host == "" {
-		host = "localhost"
-	}
 	rdb := redis.NewClient(&redis.Options{
 		Addr: fmt.Sprintf("%s:6379", host),
 	})
